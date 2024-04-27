4357f1ff769d6902444af6670bb68fe1d3c6da86
Belocherkovec <belocherkovec@gmail.com>
2024-04-27 08:10:49 +0300

    Homework 3: dev commit

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,39 +1,17 @@
-// import Header from "components/Header";
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-
-import Products from 'pages/Products';
-import About from 'pages/About';
-import Categories from 'pages/Categories';
+import { Outlet } from 'react-router-dom';
 import Header from 'components/Header';
-import ProductPage from 'pages/ProductPage';
 import PageContent from 'components/PageContent';
-
 import 'styles/style.scss';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <PageContent>
-        {/* Можно ли вынести логику навигации из App, в отдельный компонент, например <Content />. Как лучше это сделать? */}
-        <Routes>
-          <Route path="/products">
-            {/* Отображается при переходе на /products */}
-            <Route index element={<Products />} />
-
-            {/* Отображается при переходе на /products/:id */}
-            <Route path=":id" element={<ProductPage />} />
-          </Route>
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Navigate to="/products" replace />} />
-        </Routes>
+        <Outlet />
       </PageContent>
-    </BrowserRouter>
+    </>
   );
 };
 
 export default App;
-
-// background: $color-bg;
-// min-height: calc(100vh - $header-height);
